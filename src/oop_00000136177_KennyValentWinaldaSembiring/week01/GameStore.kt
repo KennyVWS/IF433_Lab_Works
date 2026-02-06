c266712685cb8c53e18dd75be81be7d23d0692fa
KennyVWS <kennyvws1@gmail.com>
2026-02-06 21:36:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000136177_KennyValentWinaldaSembiring/week01/GameStore.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week01/GameStore.kt
@@ -3,4 +3,9 @@ package oop_00000136177_KennyValentWinaldaSembiring.week01
 fun main() {
     val gameTitle: String
     val price: Int
+
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price * 20 / 100
+    else price * 10 / 100
