483ccaa094b157799480fad2a845746c2d94fca3
KennyVWS <kennyvws1@gmail.com>
2026-02-06 16:19:54 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week01/CircleCalculator.kt
@@ -0,0 +1,16 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week01
+
+fun main(args: Array<String>) {
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+
+    var area: Double = pi * radius * radius
+    println("Radius: " + radius + ", Area: " + area)
+    checkSize(area)
+}
+fun checkSize(area: Double) {
+    println("This is a Big Circle")
+} else {
+    println("This is a Small Circle")
+}
+}
