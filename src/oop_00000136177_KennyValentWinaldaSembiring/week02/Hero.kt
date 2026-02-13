83a503ed74f33ee0272bb2e024501ad73b493f25
KennyVWS <kennyvws1@gmail.com>
2026-02-13 15:51:53 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week02/Hero.kt
@@ -0,0 +1,33 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week02
+
+class Enemy (
+    val NameMusuh: String,
+    var HpEnemy: Int = 100
+)
+
+class Hero (
+    val Name: String,
+    var Hp: Int = 100,
+    var BaseDamage: Int
+)
+{
+        fun attack(targetName: String) {
+        println("${Name} menebas $targetName")
+    }
+    fun takeDamage(Damage: Int) {
+        Hp = Hp - Damage
+
+        if (Hp < 0) {
+            Hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        if (Hp > 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+}
