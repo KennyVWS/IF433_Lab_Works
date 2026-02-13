d8d1992c2704cf4cb03b9102ac82a1b7ba988c3e
KennyVWS <kennyvws1@gmail.com>
2026-02-13 12:03:42 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week02/Main.kt
@@ -0,0 +1,25 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner  = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name = name, nim = nim, major = major)
+        println("Status: Pendaftaran Selesai")
+    }
+}
--- src/oop_00000136177_KennyValentWinaldaSembiring/week02/Student.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week02/Student.kt
@@ -1,16 +1,16 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week02
 
-class student ( new*
-        val name:String,
-        val nim:String,
-        val major:String
-) { new*
-        init { new*
-        if(nim.length != 5 ) {
+class Student (
+                val name: String,
+                val nim: String,
+                val major: String
+) {
+    init {
+            if(nim.length != 5 ) {
                 println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
                 println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        }else {
+            } else {
                 println("LOG: Objek Student $name berhasil dialokasikan di memory.")
-                }
-        }
+            }
+    }
 }
