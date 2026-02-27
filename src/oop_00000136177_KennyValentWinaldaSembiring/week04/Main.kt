83271803fb599f7dda2a66e2ea47e8d46f00d3c2
KennyVWS <kennyvws1@gmail.com>
2026-02-27 11:13:05 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000136177_KennyValentWinaldaSembiring/week04/Main.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week04/Main.kt
@@ -1,14 +1,14 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week04
 
 fun main() {
-    println("--- Testing Vehicle ---")
-    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
-    generalVehicle.honk()
-    generalVehicle.accelerate()
 
-    println("\n--- Testing Car ---")
-    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()
-    myCar.honk()
-    myCar.accelerate()
+    val tesla = Car.ElectricCar(
+        brand = "Tesla",
+        numberOfDoors = 4,
+        batteryCapacity = 85
+    )
+    tesla.openTrunk()
+    tesla.honk()
+    tesla.accelerate()
+
 }
