c3dfd8fabdb12d5ce8ffa438751733e6bb992814
KennyVWS <kennyvws1@gmail.com>
2026-02-27 11:07:10 +0700

    week04: (task 1) create ElectricCar class with final override

--- src/oop_00000136177_KennyValentWinaldaSembiring/week04/Car.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week04/Car.kt
@@ -1,15 +1,26 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week04
 
-class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
     fun openTrunk() {
-            println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
     }
+
     override fun honk() {
         println("TIN! TIN! Mobil $brand lewat!")
     }
+
     override fun accelerate() {
         super.accelerate()
         println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
+    }
+
+    class ElectricCar(brand: String, numberOfDoors: Int, var batteryCapacity: Int ) : Car(brand, numberOfDoors)
+    {
+
+        final override fun accelerate() {
+            println("Mobil $brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%")
+        }
+
     }
 }
