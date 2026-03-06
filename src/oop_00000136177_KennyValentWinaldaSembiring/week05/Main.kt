487316a9d163c65052a5bad99a54cd3607ba4956
KennyVWS <kennyvws1@gmail.com>
2026-03-06 12:40:48 +0700

    week05: (task2) implement payment polymorphism

--- src/oop_00000136177_KennyValentWinaldaSembiring/week05/Main.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week05/Main.kt
@@ -1,25 +1,14 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week05
 
 fun main() {
-    val dosen1 = Dosen("Pak Alex", "0123456")
-    val admin1 = Admin(nama = "Bu Siti")
 
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+    val wallet = EWallet(50000.0, "Kenny")
+    val card = CreditCard(100000.0, accountName = "Kenny")
 
-    println("=== AKTIVITAS PEGAWAI ===")
-    for (pegawai in daftarPegawai) {
-        pegawai.bekerja()
+    val paymentMethods: List<PaymentMethod> = listOf(wallet, card)
 
-        when (pegawai) {
-            is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn}")
-                pegawai.mengajar()
-            }
-            is Admin -> {
-                println("=> Terdeteksi sebagai Admin")
-                pegawai.doAdminWork()
-            }
-        }
-        println("----------------")
+    for (method in paymentMethods) {
+        method.processPayment(75000.0)
     }
+
 }
