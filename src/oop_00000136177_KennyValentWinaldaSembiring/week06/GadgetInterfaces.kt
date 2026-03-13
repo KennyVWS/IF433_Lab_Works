136eaa6967cde32187ff006460eda12cebe8c74c
KennyVWS <kennyvws1@gmail.com>
2026-03-13 15:09:42 +0700

    week06: create camera and phone interfaces with default methods

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week06/GadgetInterfaces.kt
@@ -0,0 +1,11 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa Kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
