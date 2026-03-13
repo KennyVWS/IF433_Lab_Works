978587663fa6150e3f4a6aabf5b462d6ef717065
KennyVWS <kennyvws1@gmail.com>
2026-03-13 15:05:58 +0700

    week06: create compound interfaces

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week06/Capabilities.kt
@@ -0,0 +1,9 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week06
+
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
