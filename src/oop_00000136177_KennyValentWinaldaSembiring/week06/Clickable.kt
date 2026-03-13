789fbd951d3eaad525f3332dab29531a248cc38e
KennyVWS <kennyvws1@gmail.com>
2026-03-13 15:02:29 +0700

    week06: create Button class implementing clickable

--- src/oop_00000136177_KennyValentWinaldaSembiring/week06/Clickable.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week06/Clickable.kt
@@ -4,4 +4,10 @@ interface Clickable {
     val name: String: String
 
     fun click()
+
+    class Button(override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil diklik!")
+        }
+    }
 }
