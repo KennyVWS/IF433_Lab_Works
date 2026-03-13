a21dc1c9f0756bf3f28b14cf23fc0419a5bf0963
KennyVWS <kennyvws1@gmail.com>
2026-03-13 15:08:03 +0700

    week06: create smartwatch with multiple implementations

---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week06/Smartwatch.kt
@@ -0,0 +1,15 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week06
+
+class Smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari Perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
