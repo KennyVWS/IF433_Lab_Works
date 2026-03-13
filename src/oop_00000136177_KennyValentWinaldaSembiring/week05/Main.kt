d8ea7af8baf1405d0c41eaa811479c8d77e2d6ed
KennyVWS <kennyvws1@gmail.com>
2026-03-13 14:58:17 +0700

    week06: fix clickable interface using abstract property

--- src/oop_00000136177_KennyValentWinaldaSembiring/week05/Main.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week05/Main.kt
@@ -1,19 +1,25 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week05
 
 fun main() {
+    val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
 
-    val wallet = EWallet(50000.0, "Kenny")
-    val card = CreditCard(100000.0, accountName = "Kenny")
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
-    val paymentMethods: List<PaymentMethod> = listOf(wallet, card)
-    for (method in paymentMethods) {
-        method.processPayment(75000.0)
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
 
-        if (method is EWallet) {
-            method.topUp(50000.0)
-            method.processPayment(amount = 75000.0)
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
         }
-
+        println("-------------------------")
     }
-
 }
---
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week06/Clickable.kt
@@ -0,0 +1,7 @@
+package oop_00000136177_KennyValentWinaldaSembiring.week06
+
+interface Clickable {
+    val name: String = "Tombol Rahasia"
+
+    fun click()
+}
