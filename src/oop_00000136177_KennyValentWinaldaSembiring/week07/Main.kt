1d295a7845bd20e198613f205f0c0a38ed7ce156
KennyVWS <kennyvws1@gmail.com>
2026-03-27 17:54:21 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000136177_KennyValentWinaldaSembiring/week07/Main.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week07/Main.kt
@@ -2,8 +2,10 @@ package oop_00000136177_KennyValentWinaldaSembiring.week07
 
 fun main() {
         GameManager.startGame()
-        GameManager.startGame()
-        GameManager.startGame()
+        println(ItemRarity.LEGENDARY.dropChance)
+        val weaponStarter = Weapon.forgeStarterSword()
+        println(weaponStarter.item)
+        println(weaponStarter.durability)
 }
 
 
