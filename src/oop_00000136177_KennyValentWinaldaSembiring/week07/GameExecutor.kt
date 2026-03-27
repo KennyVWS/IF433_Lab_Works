38197d7f7bb17aef53001f8a2c90eea61d863ae0
KennyVWS <kennyvws1@gmail.com>
2026-03-27 18:04:03 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000136177_KennyValentWinaldaSembiring/week07/GameExecutor.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week07/GameExecutor.kt
@@ -2,12 +2,12 @@ package oop_00000136177_KennyValentWinaldaSembiring.week07
 
 fun processEvent(event: BattleState) {
     when (event) {
-        is BattleState.MonsterEncounter -> println("Ketemu Monster Goblin")
+        is BattleState.MonsterEncounter -> println("Ketemu Monster: ${event.monsterName}")
         is BattleState.LootDropped -> {
             val item = event.item
             println("Mendapat loot: ${item.name} [${item.rarity}]")
         }
-        is BattleState.GameOver -> println("Game Over")
+        is BattleState.GameOver -> println("Game Over: ${event.reason}")
         is BattleState.SafeZone -> println("Safe Zone")
     }
 }
--- src/oop_00000136177_KennyValentWinaldaSembiring/week07/Main.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week07/Main.kt
@@ -1,11 +1,16 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week07
 
 fun main() {
-        GameManager.startGame()
-        println(ItemRarity.LEGENDARY.dropChance)
         val weaponStarter = Weapon.forgeStarterSword()
         println(weaponStarter.item)
         println(weaponStarter.durability)
+
+        val upgradedItem = weaponStarter.item.copy(damage = 25)
+
+        processEvent(BattleState.SafeZone)
+        processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+        processEvent(BattleState.LootDropped(upgradedItem))
+        processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
 
 
