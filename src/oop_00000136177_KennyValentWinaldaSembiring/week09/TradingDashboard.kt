e91f8cc485da3c726707cb2205936345dbca5768
KennyVWS <kennyvws1@gmail.com>
2026-04-24 13:27:44 +0700

    week09: (tasK) filter closed trades only

--- src/oop_00000136177_KennyValentWinaldaSembiring/week09/TradingDashboard.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week09/TradingDashboard.kt
@@ -1,5 +1,7 @@
 package oop_00000136177_KennyValentWinaldaSembiring.week09
 
+import kotlin.collections.filter
+
 val tradeHistory: List<TradeLog> = listOf(
     TradeLog("BTCUSDT", "LONG", 10, 15.5, "CLOSED"),
     TradeLog("BTCUSDT", "SHORT", 20, -5.2, "OPEN"),
@@ -8,3 +10,5 @@ val tradeHistory: List<TradeLog> = listOf(
     TradeLog("SOLUSDT", "SHORT", 25, 20.3, "OPEN"),
     TradeLog("BTCUSDT", "LONG", 50, -10.5, "CLOSED")
 )
+    val closedTrades = tradeHistory
+        .filter { it.status == "CLOSED" }
