5c3a3d79cc15614df06a8aec88f4c6594d76108b
KennyVWS <kennyvws1@gmail.com>
2026-05-07 23:11:37 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
@@ -2,27 +2,35 @@ package oop_00000136177_KennyValentWinaldaSembiring.week10
 
 fun main() {
 
-    data class ApiResponse<T>(
-        val status: String,
-        val data: T
-    )
-
-    val coinRepo = WalletRepository<Coin>()
-    coinRepo.add(Coin("BTC", 103500.0))
-    coinRepo.add(Coin("ETH", 2500.0))
-    coinRepo.add(Coin("USDT", 1.0))
+    val coinRepo = WalletRepository<coin>()
+    coinRepo.add(coin("BTC", 103500.0))
+    coinRepo.add(coin("ETH", 2500.0))
+    coinRepo.add(coin("USDT", 1.0))
 
     val response = ApiResponse(
         "200 OK",
         coinRepo.getAll()
     )
     println("Status: ${response.status}")
-
+    println()
 
     response.data.forEach { coin ->
         println("Coin: ${coin.name}")
-        println("Price: ${coin.price}")
+        println("Balance: ${coin.balance}")
         println()
     }
-    println(response)
+    val txRepo = WalletRepository<transaction>()
+
+    txRepo.add(transaction("Buy BTC", 0.5))
+    txRepo.add(transaction("Sell ETH", 1.2))
+    txRepo.add(transaction("Transfer USDT", 300.0))
+
+
+    println("=== Transactions ===")
+
+    txRepo.getAll().forEach { tx ->
+        println("Transaction: ${tx.id}")
+        println("Amount: ${tx.amount}")
+        println()
+    }
 }
