4e995295381f5ece3791840574cd040c20d0d28d
KennyVWS <kennyvws1@gmail.com>
2026-05-07 22:57:36 +0700

    week10: (task) wrap repository data into apiresponse

--- src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
@@ -2,10 +2,20 @@ package oop_00000136177_KennyValentWinaldaSembiring.week10
 
 fun main() {
 
+    data class ApiResponse<T>(
+        val status: String,
+        val data: T
+    )
+
     val coinRepo = WalletRepository<Coin>()
     coinRepo.add(Coin("BTC", 103500.0))
     coinRepo.add(Coin("ETH", 2500.0))
     coinRepo.add(Coin("USDT", 1.0))
 
-    println(coinRepo.getAll())
+    val response = ApiResponse(
+        "200 OK",
+        coinRepo.getAll()
+    )
+
+    println(response)
 }
