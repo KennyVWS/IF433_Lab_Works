e803c528f6806fba7c04809ca4e2e307e3cd65a0
KennyVWS <kennyvws1@gmail.com>
2026-05-07 22:53:33 +0700

    week10: (task) populate coin repository

--- src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week10/CryptoDashboard.kt
@@ -3,5 +3,9 @@ package oop_00000136177_KennyValentWinaldaSembiring.week10
 fun main() {
 
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 103500.0))
+    coinRepo.add(Coin("ETH", 2500.0))
+    coinRepo.add(Coin("USDT", 1.0))
 
+    println(coinRepo.getAll())
 }
