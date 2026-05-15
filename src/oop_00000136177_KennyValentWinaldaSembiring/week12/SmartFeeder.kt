b5211b32c930798c7d2a2cdea9aa03f19a389ba8
KennyVWS <kennyvws1@gmail.com>
2026-05-15 15:36:41 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000136177_KennyValentWinaldaSembiring/week12/SmartFeeder.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week12/SmartFeeder.kt
@@ -46,6 +46,6 @@ fun main() {
         )
     }.onFailure { error ->
         println("Peringatan ke Pemilik: ${error.message}")
-        println("(Opsional: Berikan chicken jerky secara manual)")
+        println("(Opsional: Berikan chicken jerky secara manuala)")
     }
 }
