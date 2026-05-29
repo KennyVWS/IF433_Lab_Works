6b69d5d51f180cc357de3ffb053c07d0b6f7835c
KennyVWS <kennyvws1@gmail.com>
2026-05-29 16:12:27 +0700

    week14: (task) implement pricingstrategy interface for ocp

--- src/oop_00000136177_KennyValentWinaldaSembiring/week14/EcommerceTask.kt
+++ src/oop_00000136177_KennyValentWinaldaSembiring/week14/EcommerceTask.kt
@@ -2,21 +2,6 @@ package oop_00000136177_KennyValentWinaldaSembiring.week14
 
 import java.io.File
 
-//class BadOrderProcessor {
-//    private val file = File("orders.csv")
-//
-//    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-//        val finalPrice = when (customerType) {
-//            "REGULAR" -> basePrice
-//            "VIP" -> basePrice * 0.90
-//            else -> basePrice
-//        }
-//        println("Memproses pesanan $itemName seharga $finalPrice")
-//        file.appendText("$itemName, $finalPrice, $customerType\n")
-//        println("Email terkirim: Pesanan $itemName anda telah dikonfirmasi!")
-//    }
-//}
-
 interface OrderRepository {
     fun saveOrder(itemName: String, finalPrice: Double, customerType: String)
 }
@@ -25,6 +10,26 @@ interface NotificationService {
     fun sendNotification(itemName: String)
 }
 
-class SafeOrderProcessor(val repo: OrderRepository, val notifier: NotificationService) {
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
 
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double {
+        return price * 0.90
+    }
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double {
+        return price
+    }
+}
+
+class SafeOrderProcessor(val repo: OrderRepository, val notifier: NotificationService) {
+    fun processOrder(itemName: String, basePrice: Double, pricingStrategy: PricingStrategy) {
+        val finalPrice = pricingStrategy.calculate(basePrice)
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        notifier.sendNotification(itemName)
+    }
 }
